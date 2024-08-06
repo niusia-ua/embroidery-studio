533363ab7d5317363267b1795d26fdabb08861a5
Nazar Antoniuk <antoniuk.nazar09@gmail.com>
2024-08-06 15:35:59 +0300

    adds UI library

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,16 @@
 import { createApp } from "vue";
+import { PrimeVue } from "@primevue/core";
+import Aura from "@primevue/themes/aura";
+
+import "primeflex/primeflex.css";
+import "primeicons/primeicons.css";
+
 import App from "./App.vue";
 
 const app = createApp(App);
+
+app.use(PrimeVue, {
+  theme: { preset: Aura },
+});
+
 app.mount("#app");
