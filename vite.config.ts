1d511b2d15d3eb47a375ee1e6ea4205775cb29d2
Nazar Antoniuk <antoniuk.nazar09@gmail.com>
2024-08-16 15:09:37 +0300

    adds alias

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,16 @@
 /// <reference types="vitest" />
+import { fileURLToPath, URL } from "node:url";
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
 
 export default defineConfig({
   plugins: [vue()],
   clearScreen: false,
+  resolve: {
+    alias: {
+      "#": fileURLToPath(new URL("./src", import.meta.url)),
+    },
+  },
   server: {
     port: 1420,
     strictPort: true,
